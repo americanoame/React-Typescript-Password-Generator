a7c4e882c1e91178a988532b49ef71f6a3ea07f9
Americano.Dev <andrenations@gmail.com>
2024-08-09 19:54:10 -0400

    Add PasswordGenerator component to the main App component

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import  PasswordGenerator  from './components/PasswordGenerator'
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <PasswordGenerator />
+    </div>
+  )
+}
+
+export default App
