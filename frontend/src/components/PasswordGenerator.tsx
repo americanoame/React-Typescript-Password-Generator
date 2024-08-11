e829c5e34f8c3ba072f685522c3d22b4c2913f14
Americano.Dev <andrenations@gmail.com>
2024-08-11 17:50:08 -0400

    Add PasswordGenerator component with UI and props integration
    - Implemented `PasswordGenerator` component using `Options` interface.
    - Add form controls for number of characters and checkbox options for character types.
    - Integrated password display and button for generating passwords.

--- frontend/src/components/PasswordGenerator.tsx
+++ frontend/src/components/PasswordGenerator.tsx
@@ -1,6 +1,14 @@
 import { Row, Col, Form, Button } from 'react-bootstrap';
+import { Options } from '../types';
 
-const PasswordGenerator: React.FC = () => {
+interface PasswordGeneratorProps {
+  options: Options;
+  password: string;
+  handleOptionChange: (key: keyof Options, value: any) => void;
+  generatePassword: () => void;
+}
+
+const PasswordGenerator: React.FC<PasswordGeneratorProps> = ({ options, password, handleOptionChange, generatePassword }) => {
   return (
     <>
       <div className="main-text">
@@ -15,16 +23,16 @@ const PasswordGenerator: React.FC = () => {
             <Form>
               <Form.Group className="mb-3">
                 <Form.Label style={{ fontSize: '20px' }}>Number of Characters</Form.Label>
-                <Form.Control type="number" min="8" max="128" />
+                <Form.Control type="number" value={options.numberOfCharacters} onChange={(e) => handleOptionChange('numberOfCharacters', parseInt(e.target.value))} />
               </Form.Group>
 
               <small>Which options would you like?</small>
 
-              <Form.Check className="mt-3" type="checkbox" label="Include Uppercase Letters" />
-              <Form.Check type="checkbox" label="Include Lowercase Letters" />
-              <Form.Check type="checkbox" label="Include Special Characters" />
-              <Form.Check type="checkbox" label="Include Numbers" />
-              <Button variant="dark" style={{ color: 'rgb(252, 1, 1)' }} className="mt-3 w-100">
+              <Form.Check className="mt-3" type="checkbox" label="Include Uppercase Letters" checked={options.includeUpper} onChange={(e) => handleOptionChange('includeUpper', e.target.checked)} />
+              <Form.Check type="checkbox" label="Include Lowercase Letters" checked={options.includeLower} onChange={(e) => handleOptionChange('includeLower', e.target.checked)} />
+              <Form.Check type="checkbox" label="Include Special Characters" checked={options.includeSpecial} onChange={(e) => handleOptionChange('includeSpecial', e.target.checked)} />
+              <Form.Check type="checkbox" label="Include Numbers" checked={options.includeNumbers} onChange={(e) => handleOptionChange('includeNumbers', e.target.checked)} />
+              <Button variant="dark" style={{ color: 'rgb(252, 1, 1)' }} className="mt-3 w-100" onClick={generatePassword}>
                 Generate Password
               </Button>
             </Form>
@@ -33,7 +41,7 @@ const PasswordGenerator: React.FC = () => {
           <Col md={6} className=" flex-column align-items-center">
             <h6 className="text-center">PASSWORD</h6>
             <div className="password-box">
-              <p>12345678</p>
+              <p>{password}</p>
             </div>
           </Col>
         </Row>
